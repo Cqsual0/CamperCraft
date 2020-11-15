3c1f8a32ce51b81b4300eb80991aba57f5abe95c
Cqsual0 <57657245+Cqsual0@users.noreply.github.com>
2020-11-15 17:11:49 +1030

    Website

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// If you don't want the particles, change the following to false:
+const doParticles = true;
+
+
+
+
+// Don't mess with :D #Cqsual
+
+function getWidth() {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
+}
+if (doParticles) {
+	if (getWidth() < 400) $.firefly({minPixel: 1,maxPixel: 2,total: 20});
+	else $.firefly({minPixel: 1,maxPixel: 3,total: 40});
+}
+
+// This is to fetch the player count
+$(document).ready(()=>{
+  const ip = $(".sip").attr("data-ip");
+  const port = $(".sip").attr("data-port");
+
+  $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result)=>{
+    if (result.online) {
+      $(".sip").html(result.players.now);
+    } else {
+      $(".playercount").html("Server isn't online!");
+    }
+  });
+
+  setInterval(()=>{
+    $.get(`https://mcapi.us/server/status?ip=${ip}&port=${port}`, (result)=>{
+      if (result.online) {
+        $(".sip").html(result.players.now);
+      } else {
+        $(".playercount").html("Server isn't online!");
+      }
+    });
+  }, 3000);
+});
